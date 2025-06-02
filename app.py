a9af2f0b38833e71e78d148364f451f9bb0a4879
adeel <adeelwaraich27@gmail.com>
2025-06-02 12:42:07 +0500

    template rendered

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
+    subject = db.Column(db.String(100), default='No Subject')
     message = db.Column(db.Text)
 with app.app_context():
     db.create_all()
@@ -20,15 +21,18 @@ def contact():
         c = Contact(
             name = request.form['name'],
             email = request.form['email'],
+            subject = request.form['subject'],
             message = request.form['message']
             )
         db.session.add(c)
         db.session.commit()
-        return "message sent!"
+        return "Thank you for your message!"
 
     
     
     
     return render_template("contact.html")
-
+@app.route("/home")
+def home():
+    return render_template("index.html")
 app.run(debug=True)
